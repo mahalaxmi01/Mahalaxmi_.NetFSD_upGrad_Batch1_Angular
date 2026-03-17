cf82da6399dc0366b7d62e85ee228aaf46ead22d
MAHA <93853379+mahalaxmi01@users.noreply.github.com>
2026-03-17 23:04:38 +0530

    Add files via upload

---
+++ C#.Net/C#_Assignment_ExceptionHandling/Assignment_2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_ExceptionHandling
+{
+    class TicketBooking
+    {
+        static int availableTickets = 15;
+
+        public static void BookTickets(int requestedTickets)
+        {
+            if (requestedTickets > availableTickets)
+            {
+                throw new TicketException("Tickets not available!");
+            }
+            else
+            {
+                availableTickets -= requestedTickets;
+                Console.WriteLine("Booking Successful!");
+                Console.WriteLine("Tickets Booked: " + requestedTickets);
+                Console.WriteLine("Tickets Remaining: " + availableTickets);
+            }
+        }
+    }
+
+    // User-defined Exception
+    class TicketException : Exception
+    {
+        public TicketException(string message) : base(message)
+        {
+        }
+    }
+    internal class Assignment_2
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Do you want to book tickets? (yes/no)");
+                string choice = Console.ReadLine();
+
+                if (choice.ToLower() == "yes")
+                {
+                    Console.WriteLine("Enter number of tickets:");
+                    int tickets = Convert.ToInt32(Console.ReadLine());
+
+                    TicketBooking.BookTickets(tickets);
+                }
+                else
+                {
+                    Console.WriteLine("Thank you!");
+                }
+            }
+            catch (TicketException ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Input!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
