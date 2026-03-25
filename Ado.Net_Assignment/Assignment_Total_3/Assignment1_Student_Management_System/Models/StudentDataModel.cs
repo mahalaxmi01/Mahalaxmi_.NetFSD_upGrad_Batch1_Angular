66877861261b0bf4e4c6f1d2c4e72f423fed421b
MAHA <93853379+mahalaxmi01@users.noreply.github.com>
2026-03-25 12:33:33 +0530

    Add files via upload

---
+++ Ado.Net_Assignment/Assignment_Total_3/Assignment1_Student_Management_System/Models/StudentDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1_Student_Management_System.models
+{
+    internal class StudentDataModel
+    {
+        public int StudentID { get; set; }
+        public string? FirstName { get; set; }
+        public char Gender { get; set; }
+
+        public string? DateOfBirth { get; set; }
+    }
+}
