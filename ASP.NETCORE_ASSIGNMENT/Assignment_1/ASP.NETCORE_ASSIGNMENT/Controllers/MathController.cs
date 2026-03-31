a331944e5c2a1155f7bc7285dfca17811f2c3bf1
MAHA <mahalaxmi01chinni@gmail.com>
2026-03-31 14:59:27 +0530

    ASP.NETCORE_ASSIGNMENT folder

---
+++ ASP.NETCORE_ASSIGNMENT/Assignment_1/ASP.NETCORE_ASSIGNMENT/Controllers/MathController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCORE_ASSIGNMENT.Controllers
+{
+    public class MathController : Controller
+    {
+        public int Add(int a,int b)
+        {
+            return a+b;
+        }
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ ASP.NETCORE_ASSIGNMENT/Assignment_1/ASP.NETCORE_ASSIGNMENT/Controllers/TeacherController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCORE_ASSIGNMENT.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public String Details()
+        {
+            return "details";
+        }
+
+        public IActionResult Employee_Details(string name,int salary,string dept)
+        {
+            ViewData["name"] = name;
+            ViewData["salary"] = salary;
+            ViewData["dept"] = dept;
+            return View();
+        }
+    }
+}
