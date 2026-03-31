d377625acaa44f189851a33327fe4b330b6dea2c
MAHA <mahalaxmi01chinni@gmail.com>
2026-03-31 12:18:23 +0530

    intial commit

---
+++ ASP.NETCORE_ASSIGNMENT/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCORE_ASSIGNMENT.Controllers
+{
+    public class UserController : Controller
+    {
+        public String Details(String name,int age)
+        {
+            return $"Name:'{name}' age:{age}";
+        }
+    }
+}
