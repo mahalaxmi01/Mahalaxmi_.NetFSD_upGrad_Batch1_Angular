e4e3884cf704860bcb718275276a729679f6144d
MAHA <93853379+mahalaxmi01@users.noreply.github.com>
2026-03-31 12:35:33 +0530

    Delete ASP.NETCORE_ASSIGNMENT directory

--- ASP.NETCORE_ASSIGNMENT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NETCORE_ASSIGNMENT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
